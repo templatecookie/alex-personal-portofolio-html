b250492e3205fc6169a88ab89d884e1c6539f164
Monir Uddin <monir74@gmail.com>
2021-06-14 18:21:24 +0600

    Done some section

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ import './src/scss/main.scss'
 import 'bootstrap/scss/bootstrap.scss'
 
 
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
